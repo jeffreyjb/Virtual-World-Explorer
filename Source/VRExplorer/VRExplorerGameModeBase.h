8c2e96e97357af6e22db04790525b7b1b8c7d7aa
Jeff Brown <jeffjjbrown@gmail.com>
2020-05-30 13:35:10 -0700

    Initial commit of UE4 project files

---
+++ Source/VRExplorer/VRExplorerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRExplorerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VREXPLORER_API AVRExplorerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
